6e1cbe29f0abfeebecee7053cc9c66091a3319e7
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2024-11-02 12:29:04 +0530

    Login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,6 +10,41 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage(''))),
+          ),
+          Column(
+            children: [
+              const Text("Login"),
+              const Text('Please enter your valid phone number.'),
+              const Text("we will send you a 4-digit code to verify"),
+              const TextField(
+                decoration: InputDecoration(),
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text("Submit")),
+              const CircleAvatar(
+                child: Text(
+                  "OR",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const Text("Login Using"),
+              const Row(
+                children: [
+                  CircleAvatar(
+                    child: Icon(Icons.facebook),
+                  ),
+                  CircleAvatar(child: Icon(Icons.mail))
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
